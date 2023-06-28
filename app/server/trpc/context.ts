9b63189397abc6dc73587c427e3cb2981a5845c6
Johnny <johnny.altes1@gmail.com>
2023-06-28 18:45:50 +0200

    cambios

--- app/server/trpc/context.ts
+++ app/server/trpc/context.ts
@@ -1,11 +1,8 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-
 import { prisma } from "../../common/prisma";
-
 export async function createContext(ctx: trpcNext.CreateNextContextOptions) {
 	const { req, res } = ctx;
-
 	return {
 		req,
 		res,
