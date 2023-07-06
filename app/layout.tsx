b68f9e047d871378d20c53e0ee233450f6fcffee
Johnny <johnny.altes1@gmail.com>
2023-07-06 17:04:08 +0200

    Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,40 +9,15 @@ import { SplashScreen } from "expo-router";
 import { useEffect } from "react";
 import { useColorScheme } from "react-native";
 import React from "react";
-import { useState } from "react";
-import { Text } from "./components/Themed";
 import LoginScreen from "./screens/login";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
-import { trpc } from "./server/utils/trpc";
-export function App() {
-  const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-          // You can pass any HTTP headers you wish here
-        }),
-      ],
-    }),
-  );
-  return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>
-        {!loaded && <SplashScreen />}
-        {loaded && <RootLayoutNav />}
-      </QueryClientProvider>
-    </trpc.Provider>
-  );
-}
+
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
+  // Ensure that reloading on /modal keeps a back button present.
   initialRouteName: "login",
 };
 
@@ -60,7 +35,8 @@ export default function RootLayout() {
   return (
     <>
       {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
-   
+      {!loaded && <SplashScreen />}
+      {loaded && <RootLayoutNav />}
     </>
   );
 }
