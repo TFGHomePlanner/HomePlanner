1b7af325a27b0caf9a66fe8ec952ac601e2c43ff
Johnny <johnny.altes1@gmail.com>
2023-07-12 22:47:55 +0200

    Ya funciona trpc

--- app/server/trpc/context.ts
+++ app/server/trpc/context.ts
@@ -1,8 +1,6 @@
 import * as trpc from "@trpc/server";
 import { prisma } from "../../common/prisma";
 import * as trpcExpress from "@trpc/server/adapters/express";
-import express from "express";
-import { appRouter } from "./router/_app";
 
 export async function createContext(
   ctx: trpcExpress.CreateExpressContextOptions
@@ -14,15 +12,5 @@ export async function createContext(
     prisma,
   };
 }
-
-const app = express();
-app.use(
-  "/trpc",
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  })
-);
-app.listen(4000);
 
 export type Context = trpc.inferAsyncReturnType<typeof createContext>;
