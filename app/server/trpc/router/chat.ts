90543667e1c04ce5ec99ba1ca48538ba82531ea7
martalopeez <martalopeez88@gmail.com>
2023-07-18 19:07:38 +0200

    se pasa el lint

--- app/server/trpc/router/chat.ts
+++ app/server/trpc/router/chat.ts
@@ -1,27 +1,22 @@
 import { publicProcedure, router } from "../trpc";
-import { signUpSchema } from "../../../common/validation/auth";
-import * as trpc from "@trpc/server";
 import z from "zod";
-import { prisma } from "../../../common/prisma";
-import { constants } from "crypto";
-import {messageSchema,  messageCreateSchema} from "../../../common/validation/message";
-
+import { messageSchema } from "../../../common/validation/message";
 
 export const chatrouter = router({
   getAllMessages: publicProcedure
     .output(z.array(messageSchema))
     .query(async ({ ctx }) => {
-      const messages =  ctx.prisma.message.findMany({
+      const messages = ctx.prisma.message.findMany({
         select: {
           Text: true,
           Day: true,
           UserId: true,
           User: {
-            select : {
+            select: {
               name: true,
               imageprofile: true,
-            }
-          }, 
+            },
+          },
         },
       });
       const messageParse = z.array(messageSchema).parse(messages);
