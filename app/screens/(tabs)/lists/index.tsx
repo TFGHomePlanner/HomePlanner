b6e26c7b4a8f49f9520c845bb3b9a6a31c39bed4
Johnny <johnny.altes1@gmail.com>
2023-07-25 17:58:38 +0200

    inicio listas de la compra

--- app/screens/(tabs)/lists/index.tsx
+++ app/screens/(tabs)/lists/index.tsx
@@ -1,18 +1,27 @@
 import { StyleSheet } from "react-native";
 import React from "react";
-import EditScreenInfo from "../../../components/EditScreenInfo";
-import { Text, View } from "../../../components/Themed";
+import AddIcon from "react-native-vector-icons/FontAwesome5";
+import { Text, View, ScrollView } from "react-native";
 
 export default function TabListsScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Listas</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/screens/tabs/lists/index.tsx" />
+    <View style={styles.container} className="bg-[#f8f3ed] m-4">
+      <View className="bg-[#F1889f] p-3 rounded-xl flex flex-row items-center justify-start w-full">
+        <AddIcon name="plus" size={20} color="white" className="mr-2" />
+        <Text className="text-lg font-bold ml-2 text-white pl-4">Nueva lista</Text>
+      </View>
+      {/* ScrollView */}
+      <ScrollView className="p-4">
+        
+        <Text className="text-lg font-bold mb-2">Listas</Text>
+        {/* Rectángulo debajo de Listas */}
+        <View className="bg-[#F1889F] p-3 rounded-lg mb-4" />
+
+        {/* Listas cerradas */}
+        <Text className="text-lg font-bold mb-2">Listas cerradas</Text>
+        {/* Rectángulo debajo de Listas cerradas */}
+        <View className="bg-[#F1889F] p-3 rounded-lg mb-4" />
+      </ScrollView>
     </View>
   );
 }
