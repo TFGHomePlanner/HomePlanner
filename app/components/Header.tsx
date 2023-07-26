56b038c55dc85b292154e68f019324e6c5f217a3
martalopeez <martalopeez88@gmail.com>
2023-07-26 18:52:00 +0200

    Se añade icono user a cabecera

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
 
 export function Header() {
   return (
-    <View className="h-20 bg-[#F8F3ED]">
-      <Text className="mt-12 text-center text-base font-bold">homeplanner</Text>
+    <View className="mt-8 h-auto flex-row bg-[#F8F3ED] p-6">
+      <Text className="ml-4 flex-1 text-center text-base font-bold">
+        homeplanner
+      </Text>
+      <Icon name="user" size={20} />
     </View>
   );
 }
