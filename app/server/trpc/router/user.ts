97938ddedb05ed681c475505e385a2ab985811c6
martalopeez <martalopeez88@gmail.com>
2023-07-26 18:52:14 +0200

    Se crean métodos para tareas de usuario

--- app/server/trpc/router/user.ts
+++ app/server/trpc/router/user.ts
@@ -25,9 +25,7 @@ export const userRouter = router({
             email: "",
           },
         };
-      }
-
-      else if (password === user.passwordHash && email === user.email) {
+      } else if (password === user.passwordHash && email === user.email) {
         return {
           success: true,
           message: "Login successful",
@@ -80,5 +78,45 @@ export const userRouter = router({
         message: "Account created successfully",
         result: result.email,
       };
+    }),
+  getUserTasks: publicProcedure
+    .input(z.object({ userId: z.string() }).nullish())
+    .query(async ({ ctx, input }) => {
+      const tasks = await ctx.prisma.userTask.findMany({
+        where: { userId: input?.userId },
+        select: {
+          Task: {
+            select: {
+              name: true,
+              description: true,
+              frequency: true,
+              createdAt: true,
+            },
+          },
+        },
+      });
+      return tasks;
+    }),
+  assignTask: publicProcedure
+    .input(z.object({ taskId: z.string(), userId: z.string() }))
+    .mutation(async ({ ctx, input: { taskId, userId } }) => {
+      await ctx.prisma.userTask.upsert({
+        where: {
+          taskId_userId: { taskId, userId },
+        },
+        create: { taskId, userId },
+        update: {},
+      });
+      return { status: 201 };
+    }),
+  unassignTask: publicProcedure
+    .input(z.object({ taskId: z.string(), userId: z.string() }))
+    .mutation(async ({ ctx, input: { taskId, userId } }) => {
+      await ctx.prisma.userTask.delete({
+        where: {
+          taskId_userId: { taskId, userId },
+        },
+      });
+      return { status: 201 };
     }),
 });
