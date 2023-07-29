37c35720e4608181b4dd8659780001cedc5439f7
martalopeez <martalopeez88@gmail.com>
2023-07-29 19:20:45 +0200

    Cambios en grupos.

--- app/components/groups/GroupCard.tsx
+++ app/components/groups/GroupCard.tsx
@@ -5,7 +5,7 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function GroupCard({ group }: { group: IGroup }) {
   return (
-    <View className="mb-4 w-full rounded-lg bg-white px-4 py-3 shadow-md shadow-lightGray">
+    <View className="mb-4 w-full rounded-lg bg-white px-4 py-3 shadow-sm shadow-lightGray">
       <View className="flex-row justify-between">
         <Text className="text-base font-semibold">{group.name}</Text>
         <Icon name="chevron-right" size={24} />
