8968da87ac37e47013551ea5db561831b746e93d
martalopeez <martalopeez88@gmail.com>
2023-08-12 19:01:40 +0200

    Tests Login y Home

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,13 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo', '@babel/preset-env', '@babel/preset-react'],
-	plugins: [
-		'nativewind/babel',
-		require.resolve('expo-router/babel'),
-		['@babel/plugin-transform-private-property-in-object', { 'loose': true }],
-		['@babel/plugin-transform-class-properties', { 'loose': true }],
-		['@babel/plugin-transform-private-methods', { 'loose': true }],
-	  ],
-	  
-  };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'nativewind/babel',
+			require.resolve('expo-router/babel'),
+			['@babel/plugin-transform-private-property-in-object', { 'loose': true }],
+			['@babel/plugin-transform-class-properties', { 'loose': true }],
+			['@babel/plugin-transform-private-methods', { 'loose': true }],
+		],
+	};
 };
